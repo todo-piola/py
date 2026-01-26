968002ac32342125aef982ce8cbd3d5c6b2ccf74
Franco Benavides <benavidesgarciafranco@gmail.com>
2026-01-26 10:58:51 +0100

    Comprobaciones añadidas

--- RA4.- Strings, tuplas y diccionarios/ExamenRA4/Ejercicio2.py
+++ RA4.- Strings, tuplas y diccionarios/ExamenRA4/Ejercicio2.py
@@ -6,7 +6,7 @@ def mostrar_tareas():
         print(f"Prioridad {x + 1}. {y}")
 
 def insertar_tarea(nombre, posicion):   
-    tareas.insert(nombre, posicion)
+    tareas.insert(posicion, nombre)
 
 def eliminar_tarea(nombre):
     tareas.remove(nombre)
@@ -15,13 +15,19 @@ def mover_tarea(nombre, nueva_posicion):
     if(nombre in tareas):
         tarea = nombre
         tareas.remove(nombre)
-        tareas.insert(nueva_posicion, tarea)
+        tareas.insert((nueva_posicion - 1), tarea)
     else:
         print(f"Lista de tareas no contiene {nombre}")
 
 mostrar_tareas()
-#insertar_tarea("volar", 2) - Tendría que invertir el orden de los parámetro ya que el método insert
+insertar_tarea("volar", 2) # - Tendría que invertir el orden de los parámetro ya que el método insert
+print("-----------------COMPROBACIÓN DESPUES DE INSERTAR------------")
+mostrar_tareas()
 # recibe primero el índice. Eso o hacer los parámetros no posicionales cosa que no recuerdo y no tengo tiempo de buscar
 eliminar_tarea("leer")
+print("-----------------COMPROBACIÓN DESPUES DE ELIMINAR------------")
+mostrar_tareas()
+
 mover_tarea("Hacer ejercicios Python", 1)
-# mostrar_tareas() - Descomentar para comprobación final de los cambios
+print("-----------------COMPROBACIÓN DESPUES DE MOVER TAREA------------")
+mostrar_tareas()
